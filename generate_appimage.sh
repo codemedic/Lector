e65c891af24d6b1756909fe6ac9a9e48fc12848e
probonopd <probonopd@users.noreply.github.com>
2019-01-04 16:45:04 +0000

    Update generate_appimage.sh

--- generate_appimage.sh
+++ generate_appimage.sh
@@ -37,7 +37,9 @@ convert app.png -resize 512x512 $APPNAME.png
 ./Lector-*.AppImage --appimage-extract
 cp -r ./lector squashfs-root/usr/conda/lib/python3.7/site-packages/
 rm squashfs-root/usr/conda/lib/python3.7/site-packages/lector.egg-link 
-rm squashfs-root/usr/conda/lib/python3.7/site-packages/easy-install.pth 
+rm squashfs-root/usr/conda/lib/python3.7/site-packages/easy-install.pth
+( cd squashfs-root/usr/conda/bin/ ; ln -s ../plugins/platforms/ . ) # FIXME
+
 wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod +x ./appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage -g ./squashfs-root
